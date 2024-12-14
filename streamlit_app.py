40bdcd3b96635cb9a32330adf0e4f41cdb2ca8a1
igunnawan31 <igunnawan25@gmail.com>
2024-12-14 20:42:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.title('Unsupervised Learning - Machine Learning')
 st.info('K-Means Clustering with Wine Dataset')
 
 df = pd.read_csv('https://raw.githubusercontent.com/igunnawan31/data/refs/heads/main/winequality-red.csv')
+df
